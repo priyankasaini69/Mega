b46cc61877000d60cd572ef517bfccd02b5fa186
Priyanka Saini <priyankaeagle69@gmail.com>
2024-10-16 20:52:32 +0530

    checking

--- AKS-Cluster-terraform/output.tf
+++ AKS-Cluster-terraform/output.tf
@@ -1,8 +1,8 @@
-output "vm_names" {
-  value = [
-    for instance in data.azurerm_virtual_machine_scale_set.aks_vmss.instance: instance.name
-  ]
-}
+# output "vm_names" {
+#   value = [
+#     for instance in data.azurerm_virtual_machine_scale_set.aks_vmss.instance: instance.name
+#   ]
+# }
 
 output "resource_group_name" {
   value = data.azurerm_virtual_machine_scale_set.aks_vmss.resource_group_name
